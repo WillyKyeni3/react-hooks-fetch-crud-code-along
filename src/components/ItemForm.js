a41316844c2626c2cf12d96cdd4dc8fec0c1166f
willymusyoka <willykyeni@gmail.com>
2025-07-18 13:04:15 +0300

    Added useEffect to GET/POST items to server

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,8 +4,28 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // function to handle form submission
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => console.log(newItem));
+  }
+
   return (
-    <form className="NewItem">
+    // set up form to call handleSubmit on submit
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,14 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  // Add useEffect hook to fetch items from the API
+  // and set the items state
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
