a14f1a3f967b537a6f44d80644048ace3f3ab9c6
willymusyoka <willykyeni@gmail.com>
2025-07-18 18:01:37 +0300

    Added the edit and delete functionality to the ShoppingList and Item components. The code now allows users to update items in the shopping list and delete items from it. The changes include adding a delete button in the Item component, handling the delete action in the ShoppingList component, and updating the state accordingly.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -15,6 +15,26 @@ function ShoppingList() {
       .then((items) => setItems(items));
   }, []);
 
+  // add this callback function to handle incart
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+    if (item.id === updatedItem.id) {
+      return (
+        updatedItem 
+      );
+    } else {
+      return item;
+    }
+  });
+  setItems(updatedItems);
+  }
+
+  // add this callback function to handle delete item from list
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+  }
+
   // Function to handle category change
   function handleAddItem(newItem) {
   setItems([...items, newItem]);
@@ -39,7 +59,12 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
